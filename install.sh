bfb7a8119b0352f4dcb3ed08f81d5542e2922a28
nchf3 <nicolas.cros3@orange.fr>
2023-04-30 08:54:31 +0200

    create a minimal runtime

---
+++ install.sh
@@ -0,0 +1,5 @@
+# export cross-tool chain in the path
+export PATH="$PATH:/home/utilisateur/workspace/tools/riscv/bin"
+
+# export qemu in the path
+export PATH="$PATH:/home/utilisateur/workspace/projects/qemu-8.0.0/build"
