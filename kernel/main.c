1e4da1c86c0ea238287638b3315104b4b7d7d6bc
nchf3 <nicolas.cros3@orange.fr>
2023-05-26 21:04:54 +0200

    create a function to send a string instead of a byte

--- kernel/main.c
+++ kernel/main.c
@@ -17,6 +17,7 @@
 
 #define uint64_t    long unsigned int
 #define uint32_t    unsigned int
+#define uint16_t    unsigned short int
 #define uint8_t     unsigned char
 
 #define UART_BASE_ADDR          0x10000000
@@ -25,33 +26,35 @@
 #define UART_RCV_IT_OFFSET      0x00000001
 #define UART_RX_TX_OFFSET       0x00000000
 
-inline void mmio_write64(uint64_t addr, uint64_t data) {
-    // we tell the compiler to not discard this write operation
-    // by optimization
+inline void write64(uint64_t addr, uint64_t data) {
     volatile uint64_t *reg_addr = (uint64_t *)addr;
     *reg_addr = data;
 }
 
-inline uint8_t mmio_read64(uint64_t addr) {
-    // we tell the compiler to not discard this read operation
-    // by optimization
+inline uint8_t read64(uint64_t addr) {
     volatile uint64_t *reg_addr = (uint64_t *)addr;
     return (uint64_t)*reg_addr;
 }
 
 void uart_init() {
-    mmio_write64(UART_BASE_ADDR, (1 << 24) | (1 << 25) | (1 << 16) | (1 << 8));
+    write64(UART_BASE_ADDR, (1 << 24) | (1 << 25) | (1 << 16) | (1 << 8));
 }
 
 void uart_send_byte(uint8_t data) {
-    uint64_t val = mmio_read64(UART_BASE_ADDR);
+    uint64_t val = read64(UART_BASE_ADDR);
     val &= 0xFFFFFFFFFFFFFF00;
     val |= (uint64_t)data;
-    mmio_write64(UART_BASE_ADDR, val);
+    write64(UART_BASE_ADDR, val);
 }
 
 void uart_send(uint8_t *data, uint64_t size) {
+    uint32_t byte_index = 0;
 
+    while(byte_index < size) {
+        uart_send_byte(data[byte_index]);
+
+        byte_index += 1;
+    }
 }
 
 void platform_init() {
@@ -59,12 +62,10 @@ void platform_init() {
 }
 
 void kernel_init() {
-    
-    uart_send_byte('h');
-    uart_send_byte('e');
-    uart_send_byte('l');
-    uart_send_byte('l');
-    uart_send_byte('o');
+
+    unsigned char kernel_msg[] = "hello";
+
+    uart_send(kernel_msg, 6);
 
     while(1);
 }
