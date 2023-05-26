048d8cf1b4e3ebb423aadceed98219aac9672998
nchf3 <nicolas.cros3@orange.fr>
2023-05-26 22:29:48 +0200

    divide main file in dedicated modules

--- kernel/common.h
+++ kernel/common.h
@@ -20,54 +20,19 @@
 #define uint16_t    unsigned short int
 #define uint8_t     unsigned char
 
-#define UART_BASE_ADDR          0x10000000
-#define UART_LCR_OFFSET         3
-#define UART_FIFO_OFFSET        2
-#define UART_RCV_IT_OFFSET      1
-#define UART_RX_TX_OFFSET       0
-
-inline void write64(const uint64_t addr, const uint64_t data) {
+inline void reg_write_double_world(const uint64_t addr, const uint64_t data) {
     volatile uint64_t *reg_addr = (uint64_t *)addr;
     *reg_addr = data;
 }
 
-inline uint8_t read64(const uint64_t addr) {
+inline uint8_t reg_read_double_world(const uint64_t addr) {
     volatile uint64_t *reg_addr = (uint64_t *)addr;
     return (uint64_t)*reg_addr;
 }
 
-inline void write8(const uint64_t addr, const uint64_t offset, const uint64_t data) {
-    uint64_t val = read64(UART_BASE_ADDR);
+inline void register_write_byte(const uint64_t addr, const uint64_t offset, const uint64_t data) {
+    uint64_t val = reg_read_double_world(addr);
     val &= 0xFFFFFFFFFFFFFF00 << (8 * offset);
     val |= (uint64_t)data << (8 * offset);
-    write64(UART_BASE_ADDR, val);
-}
-
-void uart_init() {
-    write8(UART_BASE_ADDR, UART_RCV_IT_OFFSET, (1 << 0));
-    write8(UART_BASE_ADDR, UART_FIFO_OFFSET, (1 << 0));
-    write8(UART_BASE_ADDR, UART_LCR_OFFSET, (1 << 0) | (1 << 1));
-}
-
-void uart_send(const uint8_t *data, const uint64_t size) {
-    uint32_t byte_index = 0;
-
-    while(byte_index < size) {
-        write8(UART_BASE_ADDR, UART_RX_TX_OFFSET, data[byte_index]);
-
-        byte_index += 1;
-    }
-}
-
-void platform_init() {
-    uart_init();
-}
-
-void kernel_init() {
-
-    unsigned char kernel_msg[] = "hello";
-
-    uart_send(kernel_msg, 6);
-
-    while(1);
+    reg_write_double_world(addr, val);
 }
