d61b3d1e9c0a86b576e63aa2aaf8e86492c65898
nchf3 <nicolas.cros3@orange.fr>
2023-05-26 21:06:39 +0200

    use const in function argument

--- kernel/main.c
+++ kernel/main.c
@@ -26,12 +26,12 @@
 #define UART_RCV_IT_OFFSET      0x00000001
 #define UART_RX_TX_OFFSET       0x00000000
 
-inline void write64(uint64_t addr, uint64_t data) {
+inline void write64(const uint64_t addr, const uint64_t data) {
     volatile uint64_t *reg_addr = (uint64_t *)addr;
     *reg_addr = data;
 }
 
-inline uint8_t read64(uint64_t addr) {
+inline uint8_t read64(const uint64_t addr) {
     volatile uint64_t *reg_addr = (uint64_t *)addr;
     return (uint64_t)*reg_addr;
 }
@@ -40,14 +40,14 @@ void uart_init() {
     write64(UART_BASE_ADDR, (1 << 24) | (1 << 25) | (1 << 16) | (1 << 8));
 }
 
-void uart_send_byte(uint8_t data) {
+void uart_send_byte(const uint8_t data) {
     uint64_t val = read64(UART_BASE_ADDR);
     val &= 0xFFFFFFFFFFFFFF00;
     val |= (uint64_t)data;
     write64(UART_BASE_ADDR, val);
 }
 
-void uart_send(uint8_t *data, uint64_t size) {
+void uart_send(const uint8_t *data, const uint64_t size) {
     uint32_t byte_index = 0;
 
     while(byte_index < size) {
