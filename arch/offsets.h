9b1079ebc70b93ed723aaca12afbf152035480be
nchf3 <nicolas.cros3@orange.fr>
2023-06-22 09:53:16 +0200

    improve assembly lisibily by defining register offsets

---
+++ arch/offsets.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2023 Qoda, engineering
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms and conditions of the GNU General Public License,
+ * version 3 or later, as published by the Free Software Foundation.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+
+ * You should have received copies of the GNU General Public License and
+ * the GNU Lesser General Public License along with this program.  If
+ * not, see https://www.gnu.org/licenses/
+ */
+#ifndef OFFSETS_H
+#define OFFSETS_H
+
+#define TASK_THREAD_RA  0
+#define TASK_THREAD_SP  8
+#define TASK_THREAD_S0  16
+#define TASK_THREAD_S1  24
+#define TASK_THREAD_S2  32
+#define TASK_THREAD_S3  40
+#define TASK_THREAD_S4  48
+#define TASK_THREAD_S5  56
+#define TASK_THREAD_S6  64
+#define TASK_THREAD_S7  72
+#define TASK_THREAD_S8  80
+#define TASK_THREAD_S9  88
+#define TASK_THREAD_S10 96
+#define TASK_THREAD_S11 104
+#define TASK_THREAD_A0  112
+
+#endif
