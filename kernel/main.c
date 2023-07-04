0dd0a9a551513e0711506d6f0e9685bc8dafe516
nchf3 <nicolas.cros3@orange.fr>
2023-07-04 22:46:37 +0200

    use the init routine to initialize applications from a statically defined section in ram

---
+++ kernel/main.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2023 Qoda, engineering
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms and conditions of the GNU General Public License,
+ * version 3 or later, as published by the Free Software Foundation.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+
+ * You should have received copies of the GNU General Public License and
+ * the GNU Lesser General Public License along with this program.  If
+ * not, see https://www.gnu.org/licenses/
+ */
+
+#include "app.h"
+#include "printf.h"
+
+void main(void) {
+  printf("hello from the app\r\n");
+
+  while (1) {
+  }
+}
+
+REGISTER_APP(my_first_app, 2, main)
