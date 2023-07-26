aa76f2cabb1eb6d52c53da579a4923b6cca9f4c6
nchf3 <nicolas.cros3@orange.fr>
2023-07-26 19:20:05 +0200

    delete unused file

--- install.sh
+++
@@ -1,5 +0,0 @@
-# export cross-tool chain in the path
-export PATH="$PATH:/home/utilisateur/workspace/tools/riscv/bin"
-
-# export qemu in the path
-export PATH="$PATH:/home/utilisateur/workspace/projects/qemu-8.0.0/build"
