694ddb95f223584a1cb3f2c7f79957d30c07846b
nchf3 <nicolas.cros3@orange.fr>
2023-11-09 10:35:19 +0100

    add python scripts to the CI

---
+++ tools/ci/ci.py
@@ -0,0 +1,41 @@
+# Copyright (c) 2023 Qoda, engineering
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms and conditions of the GNU General Public License,
+# version 3 or later, as published by the Free Software Foundation.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+
+# You should have received copies of the GNU General Public License and
+# the GNU Lesser General Public License along with this program.  If
+# not, see https://www.gnu.org/licenses/
+
+# This script is used to automate the run of anckor tests on a cloud 
+# server provider
+
+from pexpect import *
+
+# create a process with the QEMU guest
+child = spawn('anckor run')
+
+# look for the output of the QEMU guest
+while True:
+    child_response = child.readline().decode('utf-8')
+    print(child_response)
+    if "ATE - PASSED" in child_response:
+        test_passed = True
+        break
+    elif "ATE - FAILED" in child_response:
+        test_passed = False
+        break
+
+if test_passed:
+    print("TEST PASSED")
+else:
+    print("TEST FAILED")
+
+# kill the Qemu guest
+child.terminate(force=True)
