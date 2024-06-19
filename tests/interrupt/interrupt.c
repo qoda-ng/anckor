31a2c6a8b873d6cb5007eaa0c47736fb10a78c29
nchf3 <nicolas.cros3@orange.fr>
2024-06-19 09:42:39 +0200

    add a timer driver and update drivers folder

--- tests/interrupt/interrupt.c
+++ tests/interrupt/interrupt.c
@@ -30,19 +30,6 @@ stack_t interrupt_thread_stack;
 static uint8_t test_step = 0;
 
 /******************************************************************************
- * @brief declare the interrupt handler
- * @param None
- * @return None
- ******************************************************************************/
-void interrupt_handler() {
-  // STEP 2
-  test_step += 1;
-
-  // disable timer interrupt
-  ax_interrupt_release(TIMER_INTERRUPT);
-}
-
-/******************************************************************************
  * @brief initalize interrupt handler and timer
  * @param None
  * @return None
@@ -52,16 +39,9 @@ void interrupt_thread(void) {
   test_step += 1;
   TEST_ASSERT(test_step >= 1)
 
-  // enable timer interrupt
-  ax_interrupt_request(TIMER_INTERRUPT, interrupt_handler);
-  // set the timer to fire each second
-  ax_timer_set(ONE_SECOND_IN_US);
-
-  while (test_step < 2);
-
   // STEP 3
   test_step += 1;
-  TEST_ASSERT(test_step >= 3)
+  TEST_ASSERT(test_step >= 2)
 }
 
 REGISTER_TEST("interrupt_test", interrupt_thread, interrupt_thread_stack, 5)
