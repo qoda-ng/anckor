0d7efdc75983d85576f2c39e351f23826474c667
nchf3 <nicolas.cros3@orange.fr>
2024-12-13 10:35:12 +0100

    use kernel.img as executable

--- tools/make/build.mk
+++ tools/make/build.mk
@@ -73,8 +73,8 @@ build: setup_build_dir $(MODULE_TARGET_LIST)
 
 run:
 	$(info run [release] build)
-	@qemu-system-riscv64 -machine virt -cpu rv64 -smp 4 -m 512M -nographic -serial mon:stdio -bios none -kernel build/kernel.elf
+	@qemu-system-riscv64 -machine virt -cpu rv64 -smp 4 -m 512M -nographic -serial mon:stdio -bios none -kernel build/kernel.img
 
 debug:
 	$(info run [debug] build)
-	@qemu-system-riscv64 -s -S -machine virt -cpu rv64 -smp 4 -m 512M -nographic -serial mon:stdio -bios none -kernel build/kernel.elf
+	@qemu-system-riscv64 -s -S -machine virt -cpu rv64 -smp 4 -m 512M -nographic -serial mon:stdio -bios none -kernel build/kernel.img
