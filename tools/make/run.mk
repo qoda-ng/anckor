62357454025a266c9d573a7fca4eb4b8c56d0e8c
nchf3 <nicolas.cros3@orange.fr>
2025-01-08 09:39:15 +0100

    uniformize terminal messages

--- tools/make/run.mk
+++ tools/make/run.mk
@@ -4,12 +4,12 @@ include tools/generated/debug.mk
 
 ifeq ($(DEBUG_FLAG), true)
 	DEBUG_OPTIONS := -s -S
-	BUILD_TYPE := [debug]
+	BUILD_TYPE := --debug
 else 
 	DEBUG_OPTIONS :=
-	BUILD_TYPE := [release]
+	BUILD_TYPE := --release
 endif
 
 run:
-	$(info run $(BUILD_TYPE) build)
+	$(info [run] $(BUILD_TYPE) build)
 	@qemu-system-riscv64 $(DEBUG_OPTIONS) -machine virt -cpu rv64 -smp 4 -m 512M -nographic -serial mon:stdio -bios none -kernel build/kernel.img
